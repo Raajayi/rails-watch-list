1d2e4d3f8c81ac291c281395be29e2747ec5091f
Ademide Ajayi <ademide.r.ajayi@gmail.com>
2023-08-18 13:08:36 +0800

    update model to validate priority

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-
+  PRIORITY_OPTIONS = [1, 2, 3].freeze
   validates :movie_id, uniqueness: { scope: :list_id }
   validates :comment, length: { minimum: 6 }
+  validates :priority, inclusion: { in: PRIORITY_OPTIONS }
 end
